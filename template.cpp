65874a6883e248aac142730325a4f9712af07ac5
StewartStudios <stevendongst@gmail.com>
2026-01-30 21:42:04 +0800

    Create template.cpp with basic structure

---
+++ template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define int long long // Include this line if problem requires "long long" data type
+using namespace std;
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    return 0;
+}
